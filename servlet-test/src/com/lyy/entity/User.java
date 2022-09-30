b849ef53689f40701f7c2b37fdbd8d049e6893d6
Ice-l11 <13794743109@163.com>
2022-09-30 16:11:39 +0800

    user实体类

---
+++ servlet-test/src/com/lyy/entity/User.java
@@ -0,0 +1,58 @@
+package com.lyy.entity;
+
+/**
+ * @ClassName User
+ * @Description User实体类
+ * @Author Ice
+ * @Date 2022/9/30 15:57
+ * @Version 1.0
+ **/
+public class User {
+
+    private String name;
+    private String password;
+    private String address;
+    private String phone;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+}
